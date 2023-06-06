7391c59e21c7d3cb8c30e8e35976ee63f7c00d6e
postall_mcs <postall_mcs@mail.ru>
2023-06-06 18:23:37 +0500

    Clone Skill
    Добавлена способность при рывке создавать свой клон, который атакует рядом стоящего противника.
    Добавлены префабы клона, на котором висит скрипт CloneSkillController.cs. В объекте SkillManager.cs висит скрипт CloneSkill.cs, в котором указывается ссылка на префаб клона, и радиус действия атаки клона.
    Для клонов создан анимационный контроллер на основе главного персонажа, с анимацией атак персонажа.

---
+++ Assets/Scripts/Skills/Clone/CloneSkill.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneSkill : Skill
+{
+    [Header("Clone info")]
+    [SerializeField] private GameObject _clonePrefab;
+    [SerializeField] private float _cloneDuration;
+    [Space]
+    [SerializeField] private bool _canAttack;
+
+    public void CreateClone(Transform clonePosition)
+    {
+        GameObject newClone = Instantiate(_clonePrefab);
+        newClone.GetComponent<CloneSkillController>().SetupClone(clonePosition, _cloneDuration, _canAttack);
+    }
+}
---
+++ Assets/Scripts/Skills/Clone/CloneSkillController.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class CloneSkillController : MonoBehaviour
+{
+    #region Components
+    private SpriteRenderer _sr;
+    private Animator _anim;
+    #endregion
+
+    #region Fields
+    [SerializeField] private float _colorLoosingSpeed;
+    [SerializeField] private Transform _attackCheck;
+    [SerializeField] private float _attackCheckRadius = .8f;
+    private float _cloneTimer;
+    private Transform _closestEnemy;
+    #endregion
+
+    public void SetupClone(Transform newTransform, float cloneDuration, bool canAttack)
+    {
+        if (canAttack)
+            _anim.SetInteger("AttackNumber", Random.Range(1, 3));
+
+        transform.position = newTransform.position;
+        _cloneTimer = cloneDuration;
+        FaceClosestTarget();
+    }
+
+    private void Awake()
+    {
+        _sr = GetComponent<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _cloneTimer -= Time.deltaTime;
+
+        if (_cloneTimer < 0)
+        {
+            _sr.color = new Color(1, 1, 1, _sr.color.a - (Time.deltaTime * _colorLoosingSpeed));
+
+            if (_sr.color.a <= 0)
+                Destroy(gameObject);
+        }
+    }
+
+    private void AnimationTrigger()
+    {
+        _cloneTimer = -.1f;
+    }
+
+    private void AttackTrigger()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_attackCheck.position, _attackCheckRadius);
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+                hit.GetComponent<Enemy>().Damage();
+        }
+    }
+
+    private void FaceClosestTarget()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 25);
+
+        float closestDistance = Mathf.Infinity;
+
+        foreach (var hit in colliders)
+        {
+            if (hit.GetComponent<Enemy>() != null)
+            {
+                float distanceToEnemy = Vector2.Distance(transform.position, hit.transform.position);
+
+                if (distanceToEnemy < closestDistance)
+                {
+                    closestDistance = distanceToEnemy;
+                    _closestEnemy = hit.transform;
+                }
+            }
+        }
+
+        if (_closestEnemy != null)
+        {
+            if (transform.position.x > _closestEnemy.position.x)
+                transform.Rotate(0, 180, 0);
+        }
+    }
+}
